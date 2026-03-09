6ee8e3126fa59a97d8eb7a09ed0563f5384d5d16
ValeriiCSharp <fox.vvb11@gmail.com>
2026-03-10 00:04:16 +0100

    Curve Asset

--- Traffic Control Simulator/Assets/OnClickJump.cs
+++ Traffic Control Simulator/Assets/OnClickJump.cs
@@ -9,12 +9,17 @@ public class OnClickJump : MonoBehaviour, IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
         //OnMouseDown();
-        transform.DOPunchRotation(new Vector3(0,0,25), 0.4f, 8, 1);
         //Click();
         //transform.DOPunchScale(Vector3.one * 0.2f, 0.3f, 8, 0.8f);
     }
 
     public void Click()
+    {
+
+    }
+
+
+    void OnMouseDown()
     {
         transform.DOKill();
 
@@ -26,17 +31,5 @@ public class OnClickJump : MonoBehaviour, IPointerClickHandler
                     .SetEase(Ease.OutBack);
             });
     }
-
-
-    // void OnMouseDown()
-    // {
-    //     Sequence seq = DOTween.Sequence();
-    //
-    //     seq.Append(transform.DOScale(1.2f, 0.15f));
-    //     seq.Join(transform.DORotate(new Vector3(0,0,20), 0.15f));
-    //
-    //     seq.Append(transform.DOScale(1f, 0.2f));
-    //     seq.Join(transform.DORotate(Vector3.zero, 0.2f));
-    // }
 
 }
